03916defd2ba460100c41e4eb11470207df934be
special <1459384963@qq.com>
2018-06-02 22:59:46 +0800

    init project

---
+++ pages/house/house.js
@@ -0,0 +1,100 @@
+// pages/house/house.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    background: [
+      "https://src.leju.com/imp/imp/deal/81/10/2/3cad6dc63859424ef986a470a7f_p7_mk7_s698X523_wm47.jpg",
+      "https://src.leju.com/imp/imp/deal/99/79/8/d65c2a68618f071f35c970ba2c8_p7_mk7_s698X523_wm47.jpg",
+      "https://src.leju.com/imp/imp/deal/5c/2d/b/33329dbeff7196fba18490202a2_p7_mk7_s698X523_wm47.jpg",
+      "https://src.leju.com/imp/imp/deal/c1/0d/f/21dcdec55222e8994b3410d4711_p7_mk7_s698X523_wm47.jpg"
+
+    ],
+    list: [
+      {
+        id: "house-baseinfo",
+        name: "小区详情",
+        pages: "housedetails"
+      },
+      {
+        id: "house-planinfo",
+        name: "待定",
+        pages: "planinfo"
+      },
+      {
+        id: "house-complementinfo",
+        name: "待定",
+        pages: "complementinfo"
+      },
+      {
+        id: "house-projectinfo",
+        name: "待定",
+        pages: "projectinfo"
+      },
+      {
+        id: "house-info",
+        name: "待定",
+        pages: ""
+      }
+    ]
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
